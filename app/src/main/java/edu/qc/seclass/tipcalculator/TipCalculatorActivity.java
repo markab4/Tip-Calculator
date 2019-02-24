ab6110e1c8420f51185b012d15b41acce9cb166d
Mark Abramov <markabramov01@gmail.com>
2019-02-24 15:47:27 -0500

    Initial Commit

---
+++ app/src/main/java/edu/qc/seclass/tipcalculator/TipCalculatorActivity.java
@@ -0,0 +1,13 @@
+package edu.qc.seclass.tipcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TipCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
